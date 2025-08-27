35b06566b7f51af69e0a26613956c606805eefa6
AKAA Jennifer <jennifer.akaa@orange.com>
2025-08-27 21:39:20 +0100

    etat views

--- web/views.py
+++ web/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.utils import ProgrammingError
+from django.core.exceptions import ObjectDoesNotExist
 from tickets.models import Event
 
+
 def home(request):
-    events = Event.objects.all().order_by('start_time')
+    try:
+        events = Event.objects.all().order_by('start_time')
+    except ProgrammingError:
+        # La table n'existe pas encore (migrations non appliquées)
+        events = []
     return render(request, 'web/home.html', {'events': events})
 
+
 def event_detail(request, event_id):
-    event = Event.objects.get(pk=event_id)
+    try:
+        event = Event.objects.get(pk=event_id)
+    except (ProgrammingError, ObjectDoesNotExist):
+        # Si la table n'existe pas ou que l'event n'existe pas
+        event = None
     return render(request, 'web/event_detail.html', {'event': event})
